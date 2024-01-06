eff50ac8e1862759d3717ccff43e30c5b3b533c9
PerfectPoipole <perfectpoipole803@gmail.com>
2024-01-06 10:40:59 -0500

    hmm

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1617,7 +1617,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 					while (learnsetidConvergence) {
 						let learnset = lsetTable.learnsets[learnsetidConvergence];
 						if (learnset) {
-							this.getTier(speciesConvergence) {
+							this.getTier(speciesConvergence) ;{
 								if (this.formatType === 'convergence') {;
 									return pokemon.num >= 0 ? String(pokemon.num) : pokemon.tier;
 								}
