bbdb50ce8ca0dbedb6cc48a274f9c67ef669b156
PerfectPoipole <perfectpoipole803@gmail.com>
2024-05-22 22:38:00 -0400

    ok

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -748,7 +748,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 	protected firstLearnsetid(speciesid: ID) {
 		let table = BattleTeambuilderTable;
 		if (this.formatType?.startsWith('bdsp')) table = table['gen8bdsp'];
-		if (this.formatType === 'nationaldexconcept') table = table['gen9nationaldexconcept'];
+		if (this.formatType === 'concept') table = table['gen9concept'];
 		if (this.formatType === 'letsgo') table = table['gen7letsgo'];
 		if (speciesid in table.learnsets) return speciesid;
 		const species = this.dex.species.get(speciesid);
@@ -807,7 +807,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 		while (learnsetid) {
 			let table = BattleTeambuilderTable;
 			if (this.formatType?.startsWith('bdsp')) table = table['gen8bdsp'];
-			if (this.formatType?.startsWith('nationaldexconcept')) table = table['gen9nationaldexconcept'];
+			if (this.formatType?.startsWith('concept')) table = table['gen9concept'];
 			if (this.formatType === 'letsgo') table = table['gen7letsgo'];
 			let learnset = table.learnsets[learnsetid];
 			if (learnset && (moveid in learnset) && (!this.format.startsWith('tradebacks') ? learnset[moveid].includes(genChar) :
@@ -829,7 +829,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 			this.formatType === 'letsgo' ? 'gen7letsgo' :
 			this.formatType === 'bdsp' ? 'gen8bdsp' :
 			this.formatType === 'bdspdoubles' ? 'gen8bdspdoubles' :
-			this.formatType === 'nationaldexconcept' ? 'gen9nationaldexconcept' :
+			this.formatType === 'concept' ? 'gen9concept' :
 			this.formatType === 'nfe' ? `gen${gen}nfe` :
 			this.formatType === 'lc' ? `gen${gen}lc` :
 			this.formatType === 'ssdlc1' ? 'gen8dlc1' :
@@ -1233,8 +1233,8 @@ class BattleItemSearch extends BattleTypedSearch<'item'> {
 			table = table['gen8bdsp'];
 		} else if (this.formatType === 'natdex') {
 			table = table['gen' + this.dex.gen + 'natdex'];
-		} else if (this.formatType === 'gen9nationaldexconcept') {
-			table = table['gen9nationaldexconcept']
+		} else if (this.formatType === 'gen9concept') {
+			table = table['gen9concept']
 		} else if (this.formatType === 'metronome') {
 			table = table['gen' + this.dex.gen + 'metronome'];
 		} else if (this.dex.gen < 9) {
@@ -1576,7 +1576,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 		let gen = '' + dex.gen;
 		let lsetTable = BattleTeambuilderTable;
 		if (this.formatType?.startsWith('bdsp')) lsetTable = lsetTable['gen8bdsp'];
-		if (this.formatType?.startsWith('nationaldexconcept')) lsetTable = lsetTable['gen9nationaldexconcept'];
+		if (this.formatType?.startsWith('concept')) lsetTable = lsetTable['gen9concept'];
 		if (this.formatType === 'letsgo') lsetTable = lsetTable['gen7letsgo'];
 		if (this.formatType?.startsWith('ssdlc1')) lsetTable = lsetTable['gen8dlc1'];
 		if (this.formatType?.startsWith('predlc')) lsetTable = lsetTable['gen9predlc'];
