36d647e3abf9d20aab0538d0e7439045e398cf5f
PerfectPoipole <perfectpoipole803@gmail.com>
2024-05-22 23:16:37 -0400

    Ok

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -748,7 +748,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 	protected firstLearnsetid(speciesid: ID) {
 		let table = BattleTeambuilderTable;
 		if (this.formatType?.startsWith('bdsp')) table = table['gen8bdsp'];
-		if (this.formatType === 'nationaldexconcept') table = table['gen9nationaldexconcept'];
+		if (this.formatType === 'bdspnatdex') table = table['gen8bdspnatdex'];
 		if (this.formatType === 'letsgo') table = table['gen7letsgo'];
 		if (speciesid in table.learnsets) return speciesid;
 		const species = this.dex.species.get(speciesid);
@@ -807,7 +807,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 		while (learnsetid) {
 			let table = BattleTeambuilderTable;
 			if (this.formatType?.startsWith('bdsp')) table = table['gen8bdsp'];
-			if (this.formatType?.startsWith('nationaldexconcept')) table = table['gen9nationaldexconcept'];
+			if (this.formatType?.startsWith('bdspnatdex')) table = table['gen8bdspnatdex'];
 			if (this.formatType === 'letsgo') table = table['gen7letsgo'];
 			let learnset = table.learnsets[learnsetid];
 			if (learnset && (moveid in learnset) && (!this.format.startsWith('tradebacks') ? learnset[moveid].includes(genChar) :
@@ -829,7 +829,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 			this.formatType === 'letsgo' ? 'gen7letsgo' :
 			this.formatType === 'bdsp' ? 'gen8bdsp' :
 			this.formatType === 'bdspdoubles' ? 'gen8bdspdoubles' :
-			this.formatType === 'nationaldexconcept' ? 'gen9nationaldexconcept' :
+			this.formatType === 'bdspnatdex' ? 'gen8bdspnatdex' :
 			this.formatType === 'nfe' ? `gen${gen}nfe` :
 			this.formatType === 'lc' ? `gen${gen}lc` :
 			this.formatType === 'ssdlc1' ? 'gen8dlc1' :
@@ -1231,10 +1231,11 @@ class BattleItemSearch extends BattleTypedSearch<'item'> {
 		let table = BattleTeambuilderTable;
 		if (this.formatType?.startsWith('bdsp')) {
 			table = table['gen8bdsp'];
+		}
+		else if (this.formatType?.startsWith('bdspnatdex')) {
+			table = table['gen8bdspnatdex'];
 		} else if (this.formatType === 'natdex') {
 			table = table['gen' + this.dex.gen + 'natdex'];
-		} else if (this.formatType === 'nationaldexconcept') {
-			table = table['gen9nationaldexconcept']
 		} else if (this.formatType === 'metronome') {
 			table = table['gen' + this.dex.gen + 'metronome'];
 		} else if (this.dex.gen < 9) {
@@ -1565,7 +1566,6 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 		const isSTABmons = (format.includes('stabmons') || format === 'staaabmons');
 		const isConvergence = format.includes('convergence');
 		const isTradebacks = format.includes('tradebacks');
-		const isNationalDexConcept = format.includes ('nationaldexconcept')
 		const regionBornLegality = dex.gen >= 6 &&
 			(/^battle(spot|stadium|festival)/.test(format) || format.startsWith('bss') ||
 				format.startsWith('vgc') || (dex.gen === 9 && this.formatType !== 'natdex'));
@@ -1577,7 +1577,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 		let gen = '' + dex.gen;
 		let lsetTable = BattleTeambuilderTable;
 		if (this.formatType?.startsWith('bdsp')) lsetTable = lsetTable['gen8bdsp'];
-		if (this.formatType?.startsWith('nationaldexconcept')) lsetTable = lsetTable['gen9nationaldexconcept'];
+		if (this.formatType?.startsWith('bdspnatdex')) lsetTable = lsetTable['gen8bdspnatdex'];
 		if (this.formatType === 'letsgo') lsetTable = lsetTable['gen7letsgo'];
 		if (this.formatType?.startsWith('ssdlc1')) lsetTable = lsetTable['gen8dlc1'];
 		if (this.formatType?.startsWith('predlc')) lsetTable = lsetTable['gen9predlc'];
@@ -1609,7 +1609,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 					}
 					if (
 						this.formatType?.includes('predlc') && this.formatType !== 'predlcnatdex' &&
-						Table['gen9predlc']?.nonstandardMoves.includes(moveid)
+						BattleTeambuilderTable['gen9predlc']?.nonstandardMoves.includes(moveid)
 					) {
 						continue;
 					}
